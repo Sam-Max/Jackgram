470c52dcc699a3dab1519d7436e5ab0ee24e14bf
sammax <dsammax@outlook.com>
2024-11-11 02:10:44 +0100

    update utils.py

--- streamtg/utils/utils.py
+++ streamtg/utils/utils.py
@@ -1,15 +1,37 @@
+import re
+
+
+def clean_file_name(name: str) -> str:
+    """Removes common and unnecessary strings from file names"""
+    reg_exps = [
+        r"\((?:\D.+?|.+?\D)\)|\[(?:\D.+?|.+?\D)\]",  # (2016), [2016], etc
+        r"\(?(?:240|360|480|720|1080|1440|2160)p?\)?",  # 1080p, 720p, etc
+        r"\b(?:mp4|mkv|wmv|m4v|mov|avi|flv|webm|flac|mka|m4a|aac|ogg)\b",  # file types
+        r"season ?\d+?",  # season 1, season 2, etc
+        # more stuffs
+        r"(?:S\d{1,3}|\d+?bit|dsnp|web\-dl|ddp\d+? ? \d|hevc|hdrip|\-?Vyndros)",
+        # URLs in filenames
+        r"^(?:https?:\/\/)?(?:www.)?[a-z0-9]+\.[a-z]+(?:\/[a-zA-Z0-9#]+\/?)*$",
+    ]
+    for reg in reg_exps:
+        name = re.sub(reg, "", name)
+    return name.strip().rstrip(".-_")
 
 
 def get_readable_size(size_in_bytes):
     size_in_bytes = int(size_in_bytes) if str(size_in_bytes).isdigit() else 0
     if not size_in_bytes:
-        return '0B'
-    index, SIZE_UNITS = 0, ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
+        return "0B"
+    index, SIZE_UNITS = 0, ["B", "KB", "MB", "GB", "TB", "PB"]
 
     while size_in_bytes >= 1024 and index < len(SIZE_UNITS) - 1:
         size_in_bytes /= 1024
         index += 1
-    return f'{size_in_bytes:.2f}{SIZE_UNITS[index]}' if index > 0 else f'{size_in_bytes:.2f}B'
+    return (
+        f"{size_in_bytes:.2f}{SIZE_UNITS[index]}"
+        if index > 0
+        else f"{size_in_bytes:.2f}B"
+    )
 
 
 def get_readable_time(seconds: int) -> str:
