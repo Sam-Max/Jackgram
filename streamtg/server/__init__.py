3ca4ebac5212cfde71ba4fb07cf4640298f8b063
sammax <dsammax@outlook.com>
2024-11-12 12:11:08 +0100

    register api routes

--- streamtg/server/__init__.py
+++ streamtg/server/__init__.py
@@ -1,7 +1,9 @@
 from streamtg.server.routes import routes
+from streamtg.server.api.streamtg_api import routes as api
 from aiohttp import web
 
 def web_server():
     web_app = web.Application(client_max_size=30000000)
     web_app.add_routes(routes)
+    web_app.add_routes(api)
     return web_app
