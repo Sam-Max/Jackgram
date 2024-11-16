0ec8c8faabece417a7016e543fb7bd6cc224d895
sammax <dsammax@outlook.com>
2024-11-16 04:37:00 +0100

    feat: create generate_string_session.py

---
+++ generate_string_session.py
@@ -0,0 +1,21 @@
+from pyrogram import Client
+from pyrogram.errors import UserIsBot
+import asyncio
+
+API_KEY = ""  # Replace with your actual API key
+API_HASH = ""  # Replace with your actual API hash
+
+
+async def generate_session_string():
+    async with Client(
+        name="StreamGram-User", api_id=API_KEY, api_hash=API_HASH, in_memory=True
+    ) as app:
+        session = await app.export_session_string()
+        try:
+            await app.send_message("me", f"#StreamGram\n\n<code>{session}</code>")
+        except UserIsBot:
+            pass
+        print(f"Done!!, String Session: {session}")
+
+
+asyncio.run(generate_session_string())
